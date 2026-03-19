664628d1a37230a8b8a913eab4137c061a61902b
kerrinna-l <thaykerrinna382@gmail.com>
2026-03-19 13:09:25 +0000

    Adicionando codespace

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/email')
+@app.route('/faleconosco')
+@app.route('/contato')
+def contato():
+    return render_template('contato.html')
+
+if __name__ == '__main__':
+    app.run()
